eba52e0aa20d8d845e418e66dd3cfdc8115c9740
Balaji Saravanan <balajilitsv@gmail.com>
2019-06-06 16:15:29 +0530

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,29 @@
+function closeFun() {
+document.getElementById("mySidenav").style.width = "0";
+}
+
+function openFun() {
+document.getElementById("mySidenav").style.width="280px";
+}
+
+function closeFun1() {
+document.getElementById("mySidenav1").style.width = "0";
+}
+
+function openFun1() {
+document.getElementById("mySidenav1").style.width="280px";
+}
+
+function closeFun2() {
+document.getElementById("mySidenav2").style.width = "0";
+}
+
+function openFun2() {
+document.getElementById("mySidenav2").style.width="280px";
+}
+
+// function hideMetrics(){
+//     document.getElementById("metrics").style.display="none";
+//     document.getElementById("modalForm").style.display="block";
+
+// }
